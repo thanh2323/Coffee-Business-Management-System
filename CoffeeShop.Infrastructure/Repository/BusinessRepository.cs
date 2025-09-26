ecbc87d60fdeb8231f8f4f8e006395fb45eb3f6a
Chi Thanh <nguyenchithanh2213@gmail.com>
2025-09-26 18:54:59 +0700

    Fix: clean up merge conflict

--- CoffeeShop.Infrastructure/Repository/BusinessRepository.cs
+++ CoffeeShop.Infrastructure/Repository/BusinessRepository.cs
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -31,39 +31,3 @@ namespace CoffeeShop.Infrastructure.Repository
         }
     }
 }
-
-=======
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CoffeeShop.Application.Interface.IRepo;
-using CoffeeShop.Domain.Entities;
-using CoffeeShop.Infrastructure.Data;
-using Microsoft.EntityFrameworkCore;
-
-namespace CoffeeShop.Infrastructure.Repository
-{
-    public class BusinessRepository : BaseRepository<Business>, IBusinessRepository
-    {
-        public BusinessRepository(ApplicationDbContext context) : base(context)
-        {
-        }
-
-        public async Task<Business?> GetByBusinessNameAsync(string businessName)
-        {
-            return await _dbSet.FirstOrDefaultAsync(b => b.Name == businessName);
-        }
-
-        public async Task<IEnumerable<Business>> GetActiveBusinessesAsync()
-        {
-            return await _dbSet.Where(b => b.IsActive).ToListAsync();
-        }
-
-        public async Task<Business?> GetByIdAsync(int businessId)
-        {
-            return await _dbSet.FirstOrDefaultAsync(b => b.BusinessId == businessId);
-        }
-    }
-}
-
->>>>>>> Stashed changes
